94d83ad3874a8a9d73741da12f2135d31f84a2c3
lcindia <lcindia@users.noreply.github.com>
2017-04-09 07:41:13 -0700

    Update script.js

--- learn/a/script.js
+++ learn/a/script.js
@@ -7,6 +7,12 @@ if (window.XMLHttpRequest) {
 request.open('GET', 'data.xml');
 request.onreadystatechange = function() {
 	if ((request.readyState===4) && (request.status===200)) {
+		console.log(request.responseXML);
+		
+		
+		
+		
+		
 		console.log(request.responseXML.getElementsByTagName('name')[1].firstChild.nodeValue);
 		
 		var items = request.responseXML.getElementsByTagName('name');
